1fb561646b72590207dbd110e8592caba44f357c
Humanarayudu <selvaganesh1997@gmail.com>
2026-01-08 11:52:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function generateCard() {
     format: "CODE128",
     width: passWidth,
     height: 80,
-    displayValue: false,
+    displayValue: true,
     fontSize: Math.min(14, 400 / password.length),
     lineColor: "#000000",
     background: "transparent",
@@ -61,7 +61,7 @@ function generateCard() {
     format: "CODE128",
     width: passWidth,
     height: 80,
-      displayValue: false,
+      displayValue: true,
     fontSize: Math.min(14, 400 / password.length),
     lineColor: "#000000",
     background: "transparent",
@@ -215,6 +215,7 @@ function downloadCardAsJPEG(cardElement, filename) {
 
   img.src = url;
 }
+
 
 
 
